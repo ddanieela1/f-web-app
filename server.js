237c90cf09d8b2c24a52702718717f5125c53a35
ddanieela1 <vdaniela.gv@gmail.com>
2022-09-14 12:52:38 -0400

    post route added

--- server.js
+++ server.js
@@ -2,24 +2,71 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
+const { response } = require('express');
+const passport = require('./config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const axios = require('axios');
 
-// console.log(SECRET_SESSION);
+const SECRET_SESSION = process.env.SECRET_SESSION;
+console.log('hi',SECRET_SESSION);
 
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-app.use(layouts);
+app.use(layouts);//ejs layouts
+
+//line 19-34 will run for each route
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+
+app.use(flash());            // flash middleware
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
+
+app.use((req,res,next)=>{
+  console.log('res locals >>>', res.locals) //.local is an object inside of the .res, key value of res object
+  //res.local allowes to store data and use data
+  res.locals.alerts =req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
 })
 
+//access to all of our auth routes GET/auth/signup POST routes
+app.use('/auth', require('./controllers/auth'));
+
+// Add this above /auth controllers
+app.get('/profile', isLoggedIn, (req, res) => {
+  const { id, name, email } = req.user.get(); 
+  res.render('profile', { id, name, email });
+});
+
+
+app.post('/signed-in', (req, res) => {
+  axios.get(`'https://motivational-quotes1.p.rapidapi.com/motivation${API_KEY}`)
+  .then(response => {
+    console.log(response.data);
+    res.render('signed-in',{quote:  response.data});
+  })
+  .catch(error=>{
+    console.log(error);
+  });
+});
+
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
+  console.log(`You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
 
 module.exports = server;
